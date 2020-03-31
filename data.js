f70e568273ee36ed1ccd7c28c9bba91c8a220087
suepark09 <suepandrice06@gmail.com>
2020-03-31 09:15:19 -0500

    3 files modified

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const data = [
   {
     id: 1,
-    image: "https://media.giphy.com/media/iWkHDNtcHpB5e/giphy.gif",
+    image: "https://media1.tenor.com/images/31c51e4dbd69624c48d1b334baf777bf/tenor.gif?itemid=15174843",
     name: "Sue Park",
     handle: "@suepark09",
     post: "Here is a post!"
@@ -19,6 +19,13 @@ export const data = [
     name: "Gigi",
     handle: "@gigitehcat",
     post: "i leik tew eat"
+  },
+  {
+    id: 4,
+    image: "https://media1.tenor.com/images/8dd2cb93f6c78e32a633a12ca075a59a/tenor.gif?itemid=15379511",
+    name: "Puglie",
+    handle: "@puglietehpug",
+    post: "here is an extra post just for fun"
   },
 ]
 
