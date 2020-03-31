bc0a11a8f9443d151fdf6831a7fbe1b93715d0b1
suepark09 <suepandrice06@gmail.com>
2020-03-31 08:22:13 -0500

    1 file modified

--- UserInfo.js
+++ UserInfo.js
@@ -3,17 +3,21 @@ import './style.css';
 
 function UserInfo(props) {
   return (
-   <div className="tweet-container">
      <li>
-      <div className="name-container">
-        <h3>{ props.name }</h3>
-        <p style={{paddingTop: "3px", paddingLeft: "5px"}}>{ props.handle }</p>
-      </div>
-     <div className="post-container">
-      <p>{ props.post }</p>
-     </div>
-    </li>
-   </div>
+        <div className="tweet-container">
+           <div>
+              <img src={ props.userPic } width="80px"/>
+           </div>
+           <div className="name-container">
+               <h3>{ props.name }</h3>
+                <p style={{paddingTop: "3px", paddingLeft: "5px"}}>{ props.handle }</p>
+          </div>
+          <div className="post-container">
+            <p>{ props.post }</p>
+           </div>
+        </div>
+      </li>
+  
   );
 }
 
