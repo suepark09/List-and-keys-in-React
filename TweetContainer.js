6b8fc83b6c06c4d992584ca6a73dcfd3dc28e729
suepark09 <suepandrice06@gmail.com>
2020-03-30 20:37:07 -0500

    2 files modified

--- TweetContainer.js
+++ TweetContainer.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './style.css';
 
 function TweetContainer(props) {
-
+  return (
+    <div className="tweet-container">
+      <h3>Sue Park</h3>
+    </div>
+  );
 }
 
 export default TweetContainer; 
