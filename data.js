34dc3b0d8095295b32584f6f13262ab269b0ea16
suepark09 <suepandrice06@gmail.com>
2020-03-30 19:58:19 -0500

    1 file modified

--- data.js
+++ data.js
@@ -1,19 +1,21 @@
 export const data = [
   {
     id: 1,
-    image: ""
+    image: "./pusheen.gif",
     name: "Sue Park",
     handle: "@suepark09",
     post: "Here is a post!"
   },
   {
     id: 2,
+    image: "./pusheen.gif",
     name: "Pocky",
     handle: "@pockytehcat",
     post: "i liek tew sleep"
   },
   {
     id: 3,
+    image: "./pusheen.gif",
     name: "Gigi",
     handle: "@gigitehcat",
     post: "i leik tew eat"
